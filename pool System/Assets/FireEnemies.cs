81377eb8a10eb618c00016eff0698ff3799eae43
Zeeshan Ali <za5620485@gmail.com>
2020-01-20 20:42:13 +0500

    jniun

---
+++ pool System/Assets/FireEnemies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEnemies : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemies"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
